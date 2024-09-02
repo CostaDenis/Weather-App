52db38947502b9d079a37013358d58512de39197
DedeDoidao <deniscostaf@gmail.com>
2024-09-02 20:06:10 -0300

    Conectando a Open Weather Api

---
+++ app/src/main/java/com/example/weatherapp/data/repository/WeatherRepository.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.data.repository
+
+import com.example.weatherapp.data.model.WeatherInfo
+
+interface WeatherRepository {
+    suspend fun getWeatherData(lat: Float, lng: Float): WeatherInfo
+}
